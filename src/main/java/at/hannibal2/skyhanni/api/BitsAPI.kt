2ad2566bdbd4b94b232f386a441c2984cba4fb4d
SeRaid <77941535+SeRaid743@users.noreply.github.com>
2024-05-13 15:35:21 +1200

    private

--- src/main/java/at/hannibal2/skyhanni/api/BitsAPI.kt
+++ src/main/java/at/hannibal2/skyhanni/api/BitsAPI.kt
@@ -235,7 +235,7 @@ object BitsAPI {
                 }
             }
             lore.matchFirst(cookieDurationPattern) {
-                val duration = at.hannibal2.skyhanni.utils.datetime.TimeUtils.getDuration(group("time"))
+                val duration = TimeUtils.getDuration(group("time"))
                 cookieBuffTime = SimpleTimeMark.now() + duration
             }
             lore.matchFirst(noCookieActiveSBMenuPattern) {
