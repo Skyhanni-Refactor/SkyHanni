93713cbff3a9db80434df6f3ea3071eaaaf7d494
ThatGravyBoat <thatgravyboat@gmail.com>
2024-05-13 19:07:35 -0230

    Move isAprilFools to DateUtils

--- src/main/java/at/hannibal2/skyhanni/utils/datetime/DateUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/datetime/DateUtils.kt
@@ -1,7 +1,23 @@
 package at.hannibal2.skyhanni.utils.datetime
 
+import at.hannibal2.skyhanni.SkyHanniMod
+import java.time.LocalDate
 import java.time.Month
+import java.time.ZoneId
 
 object DateUtils {
-    fun isDecember() = TimeUtils.getCurrentLocalDate().month == Month.DECEMBER
+
+    private const val ZONE = "UTC"
+
+    val now: LocalDate get() = LocalDate.now(ZoneId.of(ZONE))
+
+    @JvmStatic
+    fun isDecember() = now.month == Month.DECEMBER
+
+    @JvmStatic
+    fun isAprilFools() = when {
+        SkyHanniMod.feature.dev.debug.alwaysFunnyTime -> true
+        SkyHanniMod.feature.dev.debug.neverFunnyTime -> false
+        else -> now.month == Month.APRIL && now.dayOfMonth == 1
+    }
 }
