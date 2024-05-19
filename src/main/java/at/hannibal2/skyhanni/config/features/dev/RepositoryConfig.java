033efe9d2df7b12d51bf5698d633e6243a8aa023
Cal <cwolfson58@gmail.com>
2024-05-19 15:41:45 +1000

    worky

--- src/main/java/at/hannibal2/skyhanni/config/features/dev/RepositoryConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/dev/RepositoryConfig.java
@@ -1,10 +1,11 @@
 package at.hannibal2.skyhanni.config.features.dev;
 
-import at.hannibal2.skyhanni.SkyHanniMod;
+import at.hannibal2.skyhanni.data.repo.RepoUtils;
 import com.google.gson.annotations.Expose;
 import io.github.notenoughupdates.moulconfig.annotations.Accordion;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorButton;
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorText;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 
 public class RepositoryConfig {
@@ -15,9 +16,9 @@ public class RepositoryConfig {
     @ConfigEditorBoolean
     public boolean repoAutoUpdate = true;
 
-    @ConfigOption(name = "Update Repo Now", desc = "Update your repository to the latest version.")
+    @ConfigOption(name = "Update Repo Now", desc = "Update your repository to the latest version")
     @ConfigEditorButton(buttonText = "Update")
-    public Runnable updateRepo = SkyHanniMod.repo::updateRepo;
+    public Runnable updateRepo = RepoUtils::updateRepo;
 
     @Expose
     @ConfigOption(name = "Repository Location", desc = "")
@@ -28,18 +29,21 @@ public class RepositoryConfig {
 
         @ConfigOption(name = "Reset Repository Location", desc = "Reset your repository location to the default.")
         @ConfigEditorButton(buttonText = "Reset")
-        public Runnable resetRepoLocation = SkyHanniMod.repo::resetRepositoryLocation;
+        public Runnable resetRepoLocation = RepoUtils::resetRepoLocation;
 
         @Expose
         @ConfigOption(name = "Repository User", desc = "The Repository Branch, default: hannibal002")
+        @ConfigEditorText
         public String user = "hannibal002";
 
         @Expose
         @ConfigOption(name = "Repository Name", desc = "The Repository Name, default: SkyHanni-Repo")
+        @ConfigEditorText
         public String name = "SkyHanni-Repo";
 
         @Expose
         @ConfigOption(name = "Repository Branch", desc = "The Repository Branch, default: main")
+        @ConfigEditorText
         public String branch = "main";
     }
 
