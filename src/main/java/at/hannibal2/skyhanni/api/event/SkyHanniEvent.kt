ecb3902ed5ae5b38c7b7ee986a3b4b5ba73afd24
ThatGravyBoat <thatgravyboat@gmail.com>
2024-05-20 00:08:17 -0230

    protected

--- src/main/java/at/hannibal2/skyhanni/api/event/SkyHanniEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/api/event/SkyHanniEvent.kt
@@ -3,7 +3,7 @@ package at.hannibal2.skyhanni.api.event
 abstract class SkyHanniEvent protected constructor() {
 
     var isCancelled: Boolean = false
-        internal set
+        protected set
 
     fun post(onError: (Throwable) -> Unit = {}) {
         SkyHanniEvents.getEventHandler(javaClass).post(this, onError)
