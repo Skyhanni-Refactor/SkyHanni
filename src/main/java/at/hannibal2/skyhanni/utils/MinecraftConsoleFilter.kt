4f337fcbe33f01d1607d133c9a2e20d8d8fa97b1
ThatGravyBoat <thatgravyboat@gmail.com>
2024-05-24 11:30:03 -0230

    make filter extend AbstractFilter

--- src/main/java/at/hannibal2/skyhanni/utils/MinecraftConsoleFilter.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/MinecraftConsoleFilter.kt
@@ -10,9 +10,13 @@ import org.apache.logging.log4j.core.Filter
 import org.apache.logging.log4j.core.LogEvent
 import org.apache.logging.log4j.core.Logger
 import org.apache.logging.log4j.core.LoggerContext
+import org.apache.logging.log4j.core.filter.AbstractFilter
 import org.apache.logging.log4j.message.Message
 
-class MinecraftConsoleFilter(private val loggerConfigName: String) : Filter {
+class MinecraftConsoleFilter(private val loggerConfigName: String) : AbstractFilter(
+    Filter.Result.ACCEPT,
+    Filter.Result.DENY,
+) {
 
     private val config get() = SkyHanniMod.feature.dev.minecraftConsoles
     private val filterConfig get() = config.consoleFilter
@@ -33,10 +37,6 @@ class MinecraftConsoleFilter(private val loggerConfigName: String) : Filter {
             }
         }
     }
-
-    // prevents error sending on every shutdown
-    //TODO why does this solve the error, is there a better way to do this
-    fun stop() {}
 
     override fun filter(event: LogEvent?): Filter.Result {
         if (event == null) return Filter.Result.ACCEPT
@@ -203,14 +203,6 @@ class MinecraftConsoleFilter(private val loggerConfigName: String) : Filter {
         if (config.printFilteredReason) {
             SkyHanniMod.logger.info("filtered console: $message")
         }
-    }
-
-    override fun getOnMismatch(): Filter.Result {
-        return Filter.Result.DENY
-    }
-
-    override fun getOnMatch(): Filter.Result {
-        return Filter.Result.ACCEPT
     }
 
     override fun filter(
