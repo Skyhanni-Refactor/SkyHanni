fa107643e7a8651f29903402ace1070fe5d9022e
ThatGravyBoat <thatgravyboat@gmail.com>
2024-05-26 22:02:11 -0230

    Move to plugin to correct folder

--- .live-plugins/event/plugin.kts
+++ .live-plugins/event/plugin.kts
@@ -1,5 +1,6 @@
 import com.intellij.codeInspection.LocalQuickFix
 import com.intellij.codeInspection.ProblemDescriptor
+import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElementVisitor
@@ -11,6 +12,7 @@ import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.nj2k.postProcessing.type
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtVisitorVoid
+import org.jetbrains.kotlin.psi.psiUtil.isPublic
 import org.jetbrains.kotlin.types.typeUtil.supertypes
 
 // depends-on-plugin org.jetbrains.kotlin
@@ -25,25 +27,21 @@ class HandleEventInspectionKotlin : AbstractKotlinInspection() {
 
         val visitor = object : KtVisitorVoid() {
             override fun visitNamedFunction(function: KtNamedFunction) {
-                if (function.valueParameters.size == 1) {
-                    val parameter = function.valueParameters[0]
-                    val supers = parameter.type()?.supertypes() ?: return
-                    val hasEventAnnotation = function.annotationEntries.any { it.shortName!!.asString() == handleEvent }
-                    if (!supers.any { it.fqName?.asString() == skyhanniEvent }) {
-                        if (hasEventAnnotation) {
-                            holder.registerProblem(
-                                function,
-                                "Function should not be annotated with @HandleEvent if it does not take a SkyHanniEvent"
-                            )
-                        } else {
-                            return
-                        }
-                    }
-                    if (hasEventAnnotation) return
+                val hasEventAnnotation = function.annotationEntries.any { it.shortName!!.asString() == handleEvent }
+                val isEvent = function.valueParameters.firstOrNull()?.type()?.supertypes()
+                    ?.any { it.fqName?.asString() == skyhanniEvent } ?: false
+
+                if (isEvent && !hasEventAnnotation && function.valueParameters.size == 1 && function.isPublic) {
                     holder.registerProblem(
                         function,
                         "Event handler function should be annotated with @HandleEvent",
                         HandleEventQuickFix()
+                    )
+                } else if (!isEvent && hasEventAnnotation) {
+                    holder.registerProblem(
+                        function,
+                        "Function should not be annotated with @HandleEvent if it does not take a SkyHanniEvent",
+                        ProblemHighlightType.GENERIC_ERROR
                     )
                 }
             }
@@ -51,6 +49,7 @@ class HandleEventInspectionKotlin : AbstractKotlinInspection() {
 
         return visitor
     }
+
     override fun getDisplayName() = "Event handler function should be annotated with @HandleEvent"
     override fun getShortName() = "HandleEventInspection"
     override fun getGroupDisplayName() = "SkyHanni"
@@ -66,7 +65,7 @@ class HandleEventQuickFix : LocalQuickFix {
             null,
             null,
             { null },
-                " ",
+            " ",
             null
         )
     }
